b00c3ce8bcb3bcdaa2d66ddf674b084feae31f81
Gabe Swenson <gswen75@icloud.com>
2024-10-01 11:47:32 -0600

    first commit being made

---
+++ src/Week03Assignment/Week03.java
@@ -0,0 +1,18 @@
+package Week03Assignment;
+
+public class Week03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// create double variables with values
+		
+		double val1 = 88.00;
+		double val2 = 18.99;
+		
+		// add the values and print
+		System.out.println(val1 + val2);
+
+	}
+
+}
